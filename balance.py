5c07ac169db74d0ffa1173821f7348fd8becd6c2
jakehdchoi <jakehdchoi@gmail.com>
2018-01-10 12:24:37 +0900

    symbol list moved to symbol_list.py

--- balance.py
+++ balance.py
@@ -8,17 +8,6 @@ import json
 import config
 import symbol_lists
 
-### note
-# crontab version
-
-### todo
-# get_open_orders()를 반복할 필요 없음
-# apply_lot_size()를 반복할 필요 없음
-# test 구현
-# symbol이 이미 존재한다면 pass 하는 로직 구현 필요
-# 시간대별로 현재 자산가치(BTC)를 파일에 기록하는 로직이 필요
-# api 동작이 실패하면 다시 시도하는 로직 필요 (모든 함수에 대해서)
-
 
 api_key = config.api_key
 api_secret = config.api_secret
@@ -38,7 +27,7 @@ recvWindow=10000
 
 def main():
     print('Starting binancebot...')
-    time.sleep(10)
+    # time.sleep(10)
     print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
     global endTime, startTime
@@ -51,44 +40,14 @@ def main():
     # print(current_candle)
 
     # 현재 balance를 볼 수 있는 로직
-    # for symbol in symbol_lists:
-    #     # 밸런스를 받아오지 못 하는 경우는 어떻게 처리해야 하지.. 재시도 로직?
-    #     print(get_total_balance(symbol))
-
     for symbol in symbol_lists:
-        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-        check_latest_candle_update(symbol, interval, endTime)
-        open_orders = get_open_orders(symbol)
+        # 밸런스를 받아오지 못 하는 경우는 어떻게 처리해야 하지.. 재시도 로직?
+        print(get_total_balance(symbol))
 
-        if open_orders is None or len(open_orders) == 0:
-            pass
-        else:
-            cancel_order(symbol, open_orders[0]['orderId'])
-            if open_orders[0]['side'] == 'BUY':
-                amount = float(open_orders[0]['origQty']) - float(open_orders[0]['executedQty'])
-                rate = float(current_candle[symbol][0][4]) * 1.1
-                print(buy_limit(symbol, amount, rate))
-                print(symbol + ': buy_limit x1.1 done!')
-            elif open_orders[0]['side'] == 'SELL':
-                amount = float(open_orders[0]['origQty']) - float(open_orders[0]['executedQty'])
-                rate = float(current_candle[symbol][0][4]) * 0.9
-                print(sell_limit(symbol, amount, rate))
-                print(symbol + ': sell_limit x0.9 done!')
-            else:
-                pass
 
-        # 봇을 재부팅하면 당연히 TRUE가 되는데, 그 때 하필 30분봉이 SMA를 넘어서는 코인이 있다면 거래가 중복될 것이다
-        # 일단은 봇을 29분에 끄고 31분에 켜는 것을 원칙으로 한다
-        sma = calculate_sma(symbol, interval, startTime, endTime, period)
-        # 이전봉 close 값과 비교하는게 더 좋겠다
-        if float(current_candle[symbol][0][1]) < sma and float(current_candle[symbol][0][4]) > sma:
-            print(buy_limit(symbol, get_quantity_to_buy(sma), float(current_candle[symbol][0][4])))
-            print(symbol + ': buy_limit done!')
-        elif float(current_candle[symbol][0][4]) < sma:
-            print(sell_limit_all(symbol, float(current_candle[symbol][0][4])))
-            print(symbol + ': sell_limit done!')
-        else:
-            pass
+    # account = signed_request('https://www.binance.com/api/v3/account?', '')
+    # print('')
+    # print(account)
 
 
 def timestamp():
